e42fb65c3af72e044979a8326ae8a89544626b8b
Pierre Deville <tuxihf@gmail.com>
2016-06-03 02:57:18 +0200

    Add zprezto

--- install-script.sh
+++ install-script.sh
@@ -1,3 +1,12 @@
 #/bin/sh
 
 RCRC=rcrc rcup
+
+ln -s ~/.dotfiles/zprezto/runcoms/zlogin ~/.zlogin
+ln -s ~/.dotfiles/zprezto/runcoms/zlogout ~/.zlogout
+ln -s ~/.dotfiles/zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/.dotfiles/zprezto/runcoms/zprofile ~/.zprofile
+ln -s ~/.dotfiles/zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.dotfiles/zprezto/runcoms/zshrc ~/.zshrc
+
+chsh -s /usr/bin/zsh
